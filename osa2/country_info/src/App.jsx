3e7d91a1d96e5ce921e33ed4ba03fce5ee537e70
acpeltol <apeltola717@gmial.com>
2024-10-24 23:40:13 +0300

    I added exercises for part 3

--- osa2/country_info/src/App.jsx
+++ osa2/country_info/src/App.jsx
@@ -1,34 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState , useEffect} from 'react'
+import axios from 'axios'
+
+const Country = ({country, single}) => {
+  console.log(country)
+  return(
+    <>
+    <p>{country.name.common}
+    <button onClick={() => single(country)}>show</button>
+    </p>
+    </>
+  )
+}
+
+const Countries = ({countries, inWord, single}) => {
+
+  //const lenght = countries.map(resp => resp.name.common)
+
+  const str = countries.filter(i => i.name.common.includes(inWord))
+
+  console.log(str)
+  if (str.length > 10){
+    return(
+      <>
+      <p>Too many matches, specify another filter</p>
+      </>
+    )
+  }else if (str.length > 1){
+    console.log("hier gut")
+    return(
+      <>
+      {str.map(country => 
+      <Country key = {country.name.common} country = {country} single={single}/>
+      )}
+      </>
+    )
+  }else if (str.length === 1){
+      return(
+        <>
+        
+        <h1>{str[0].name.common}</h1>
+
+        <p>capital {str[0].capital[0]}</p>
+
+        <p>area {str[0].area}</p>
+
+        <h2>languages: </h2>
+
+        <ul>
+          {Object.entries(str[0].languages).map(([key,value]) => 
+          <li key = {key}> {value}</li>
+          )}
+        </ul>
+
+        <div className = "Flag">
+        {str[0].flag}
+        </div>
+
+
+
+
+
+        </>
+      )
+  }
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [inWord,setinWOrd] = useState('')
+  const [countries, setCountries] = useState([])
+  
+  useEffect(() => {
+    axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
+    .then(response => response.data)
+    .then(response =>
+      setCountries(response)
+    )
+  }, [])
+
+  const inWordchange = (event) => {
+    console.log(event.target.value)
+    setinWOrd(event.target.value)
+  }
+
+  const single = (country) =>{
+    console.log(country)
+    setinWOrd(country.name.common)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <p>found countries  <input value= {inWord}
+       onChange = {inWordchange}/></p>
+    </div>
+
+
+    <Countries countries={countries} single={single} inWord={inWord}/>
+      </>
   )
 }
 
